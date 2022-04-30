227ff94f44fbba3d668c7e2b9632312306e6de98
parsariyahi <pany.parsariyahi@gmail.com>
2022-04-30 12:27:45 +0430

    added fake data

--- school_manager/dashboard/views.py
+++ school_manager/dashboard/views.py
@@ -5,14 +5,14 @@ from django.http import JsonResponse
 from django.views import View
 
 
-from faker import Faker
-from random import randint, choice
-from datetime import time
+# from faker import Faker
+# from random import randint, choice, choices
+# from datetime import time
 
-from classes.models import Class
-from student.models import Student
-from teacher.models import Teacher
-from book.models import Book
+# from classes.models import Class
+# from student.models import Student
+# from teacher.models import Teacher
+# from book.models import Book
 
 
 def index(request) :
@@ -22,54 +22,58 @@ def index(request) :
     return render(request, template_name, context)
 
 def test(request) :
-    faker = Faker()
+    # faker = Faker()
 
-    t = randint(7, 16)
+    # t = randint(7, 16)
 
-    students = []
-    books = []
-    teachers = []
-    classes = []
+    # students = Student.objects.all()[15:40]
+    # books = Book.objects.all()
+    # teachers = Teacher.objects.all()
+    # for _ in range(50):
+    #     student = {
+    #         'national_id': faker.random_int(10000000, 99999999),
+    #         'name': faker.first_name(),
+    #         'last_name': faker.last_name(),
+    #         'phone_number': '09' + str(faker.random_int(1000000000, 9999999999)),
+    #     }
+    #     s = Student(**student)
+    #     s.save()
+    #     students.append(s)
+    # for _ in range(10) :
+    #     teacher = {
+    #         'personal_id': faker.random_int(10000000, 99999999),
+    #         'name': faker.first_name(),
+    #         'last_name': faker.last_name(),
+    #         'phone_number': '09' + str(faker.random_int(1000000000, 9999999999)),
+    #         'email': faker.first_name() + '@gmail.com',
+    #     }
+    #     t = Teacher(**teacher)
+    #     t.save()
+    #     teachers.append(teacher)
+    # for _ in range(20) :
+    #     book = {
+    #         'name': ' '.join(faker.words(4)),
+    #     }
+    #     b = Book(**book)
+    #     b.save()
+    #     books.append(book)
+    # r = randint(1, 10)
+    # for _ in range(4) :
+    #     class_ = {
+    #         'name': ' '.join(faker.words(4)),
+    #         'start_time': time(t, 0, 0),
+    #         'end_time': time(t+2, 0, 0),
+    #         'book': books[8],
+    #         'teacher': teachers[2],
+    #     }
+    #     c = Class(**class_)
+    #     c.save()
+        # c.students.set(students)
+    # data = {
+    #     's': students,
+    #     't': teachers,
+    #     'b': books,
+    #     'c': classes,
+    # }
 
-    for _ in range(50):
-        student = {
-            'national_id': faker.random_int(10000000, 99999999),
-            'name': faker.first_name(),
-            'last_name': faker.last_name(),
-            'phone_number': '09' + str(faker.random_int(1000000000, 9999999999)),
-        }
-        s = Student.
-        students.append(student)
-
-        teacher = {
-            'personal_id': faker.random_int(10000000, 99999999),
-            'name': faker.first_name(),
-            'last_name': faker.last_name(),
-            'phone_number': '09' + str(faker.random_int(1000000000, 9999999999)),
-            'email': faker.first_name() + '@gmail.com',
-        }
-        teachers.append(teacher)
-
-        book = {
-            'name': ' '.join(faker.words(4)),
-        }
-        books.append(book)
-
-        class_ = {
-            'name': ' '.join(faker.words(4)),
-            'start_time': time(t, 0, 0),
-            'end_time': time(t+2, 0, 0),
-            'book': choice(books),
-            'teacher': choice(teachers),
-            'students': students,
-        }
-        classes.append(class_)
-
-    data = {
-        's': students,
-        't': teachers,
-        'b': books,
-        'c': classes,
-    }
-
-    return JsonResponse(data)
+    return JsonResponse({'somm': 'dsfasfdsf'})

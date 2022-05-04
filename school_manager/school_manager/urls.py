e18bd0018aa492d16d41a698330e118ca1d23111
parsariyahi <pany.parsariyahi@gmail.com>
2022-05-04 17:57:51 +0430

    apps: compelted teacher routes and templates

--- school_manager/school_manager/urls.py
+++ school_manager/school_manager/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', include('dashboard.urls')),
     path('student/', include('student.urls')),
     path('class/', include('classes.urls')),
     path('book/', include('book.urls')),
+    path('teacher/', include('teacher.urls')),
 ]

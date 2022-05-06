81649862d566d87c2905588c072ed2c39325fd80
parsariyahi <pany.parsariyahi@gmail.com>
2022-05-06 22:08:12 +0430

    added: user auth (register, login)

--- school_manager/user/views.py
+++ school_manager/user/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import (
+    StudentRegisterForm, 
+    TeacherRegisterForm,
+)
 
 
 MAIN_TEMPLATE_DIR='user'
@@ -18,23 +23,61 @@ def logout_view(request) :
 def student_login(request):
     template_name = f'{MAIN_TEMPLATE_DIR}/student_login.html'
     context = {}
-    
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user :
+            login(request, user)
+            return redirect('dashboard:index')
+
     return render(request, template_name, context)
 
 def student_register(request):
     template_name = f'{MAIN_TEMPLATE_DIR}/student_register.html'
     context = {}
     
+    if request.method == 'POST' :
+
+        form = StudentRegisterForm(request.POST)
+        if form.is_valid() :
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard:index')
+
+    else :
+        form = StudentRegisterForm()
+        context['form'] = form
+
     return render(request, template_name, context)
 
 def teacher_login(request):
     template_name = f'{MAIN_TEMPLATE_DIR}/teacher_login.html'
     context = {}
     
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None :
+            login(request, user)
+            return redirect('dashboard:index')
+
     return render(request, template_name, context)
 
 def teacher_register(request):
     template_name = f'{MAIN_TEMPLATE_DIR}/teacher_register.html'
     context = {}
     
+    if request.method == 'POST' :
+        form = TeacherRegisterForm(request.POST)
+        if form.is_valid() :
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard:index')
+
+    else :
+        form = TeacherRegisterForm()
+        context['form'] = form
+
     return render(request, template_name, context)
